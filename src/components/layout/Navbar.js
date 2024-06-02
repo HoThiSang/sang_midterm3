2d6e842d7322403c4612162db0a4c8e54a857a82
thisang <sang.ho25@student.passerellesnumeriques.org>
2024-06-02 10:12:21 +0700

    create theme model

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -13,7 +13,13 @@ const Navbar = ({theme, toggleTheme }) => {
           <Link to="/about">About</Link>
         </li>
         <li>
-            <button onClick={toggleTheme} className="button-model">{theme ==='dark' ? (<i className="fa-solid fa-sun icon-button-light"></i>) : (<i className="fa-solid fa-moon icon-button-dark"></i> )}</button>
+            <button onClick={toggleTheme} className="button-model">
+            {theme ==='dark' ? (
+              <i className="fa-solid fa-sun icon-button-light"></i>
+              ) : (
+                <i className="fa-solid fa-moon icon-button-dark"></i> 
+                )
+                }</button>
         </li>
       </ul>
     </nav>
